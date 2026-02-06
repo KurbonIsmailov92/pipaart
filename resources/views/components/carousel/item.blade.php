9a5d9d5bb3f448ea88734471522192ac34aa6142
Kurbon <77341226+KurbonIsmailov92@users.noreply.github.com>
2026-02-06 16:09:34 +0500

    Improve mobile front-end responsiveness

--- resources/views/components/carousel/item.blade.php
+++ resources/views/components/carousel/item.blade.php
@@ -1,7 +1,7 @@
 @props(['image', 'title', 'isActive' => false])
 
 <div class="carousel-item {{ $isActive ? 'active' : '' }} position-relative">
-    <img src="{{ $image }}" class="d-block w-100 rounded-xl" alt="{{ $title }}">
+    <img src="{{ $image }}" class="d-block w-100 rounded-xl" alt="{{ $title }}" loading="{{ $isActive ? 'eager' : 'lazy' }}" decoding="async">
     <div
         class="carousel-caption d-none d-md-block bg-white/50 rounded-xl position-absolute pb-2"
         style="left: 20px; bottom: 20px; max-width: 50%;"
@@ -9,7 +9,6 @@
         <h1 class="ml-4 font-bold text-left text-blue-900 text-2xl sm:text-4xl">{{ $title }}</h1>
     </div>
 </div>
-
 
 
 
